dac6f6b95bb90fb00fb516f2523112bea2558e7e
Anton <69795966+a000011@users.noreply.github.com>
2023-11-25 10:10:27 -0800

    add statistic button (#27)

--- src/view/admin/index.ts
+++ src/view/admin/index.ts
@@ -1,5 +1,7 @@
 import { Markup, Scenes } from 'telegraf';
 import { InformerContext } from '@view/context';
+import { User } from '@models/all';
+import { MoreThan } from 'typeorm';
 
 export const SceneAdmin = new Scenes.BaseScene<InformerContext>(
 	'admin',
@@ -8,12 +10,33 @@ export const SceneAdmin = new Scenes.BaseScene<InformerContext>(
 SceneAdmin.enter(async (ctx) => {
 	await ctx.reply(
 		'Придумайте',
-		Markup.keyboard([['Создать рассылку'], ['Назад']]).resize(),
+		Markup.keyboard([
+			['Создать рассылку'],
+			['Показать статистику'],
+			['Назад'],
+		]).resize(),
 	);
 });
-
 SceneAdmin.hears('Создать рассылку', (ctx) => {
 	ctx.navigator.goto('MessageConstructor');
+});
+SceneAdmin.hears('Показать статистику', async (ctx) => {
+	const lastMonthDate = new Date();
+	lastMonthDate.setMonth(lastMonthDate.getMonth() - 1);
+
+	const usersLastMonth = await User.count({
+		where: {
+			created_at: MoreThan(lastMonthDate),
+		},
+	});
+	const usersCountAlltime = await User.count();
+
+	await ctx.reply(
+		'📈 Общее кол-во пользователей: ' +
+			usersCountAlltime +
+			'\n📈 Новых пользователей за последний месяц: ' +
+			usersLastMonth,
+	);
 });
 SceneAdmin.hears('Назад', (ctx) =>
 	ctx.navigator.goto('MainMenu'),
