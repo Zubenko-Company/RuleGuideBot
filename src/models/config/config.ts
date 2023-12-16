89604345a7def0b647a42cb740c8253f908678b0
a000011 <anton.nadikin@gmail.com>
2023-12-16 19:02:23 +0400

    fix: add portainer env

--- src/models/config/config.ts
+++ src/models/config/config.ts
@@ -1,5 +1,9 @@
 import dotenv from 'dotenv';
+import path from 'path';
 dotenv.config();
+dotenv.config({
+	path: path.join(process.cwd(), 'stack.env'),
+});
 
 const getErrorEnvMessage = (fieldName: string) =>
 	`Вы не установили ${fieldName} поле в .env, выполните команду \`cp .env.dist .env\` и заполните поле ${fieldName}`;
